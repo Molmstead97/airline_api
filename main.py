d00198c1341eaae21574df6ce967e011e623bfb4
Michael Olmstead <molmstead4876@xmail.dixietech.edu>
2024-06-19 21:54:22 -0700

    Another one

--- main.py
+++ main.py
@@ -39,32 +39,36 @@ async def get_flight_info(airline_name: str, flight_num: str):
     
     raise HTTPException(status_code=404, detail='Flight not found')
 
-@app.post("/{airline}", response_model=Airline)
-async def create_airline(new_airline: Airline):
+@app.post("/{airline}")
+async def create_flight(airline: str, new_flight: Flight):
     
-    airline_list.append(new_airline)
-    return new_airline
+    for airline_obj in airline_list:
+        if airline_obj.name == airline:
+            airline_obj.flights.append(new_flight)
+            return new_flight
+    
+    raise HTTPException(status_code=404, detail="Airline not found")
 
 @app.put("/{airline}/{flight_num}")
-async def update_flight(airline_name: str, flight_num: str, updated_flight: Flight):
+async def update_flight(airline: str, flight_num: str, updated_flight: Flight):
     
-    for airline in airline_list:
-        if airline.name == airline_name:
-            for i, flight in enumerate(airline.flights):
+    for airline_obj in airline_list:
+        if airline_obj.name == airline:
+            for i, flight in enumerate(airline_obj.flights):
                 if flight.flight_num == flight_num:
-                    airline.flights[i] = updated_flight
+                    airline_obj.flights[i] = updated_flight
                     return updated_flight
    
     raise HTTPException(status_code=404, detail='Flight not found')
 
 @app.delete("/{airline}/{flight_num}")
-async def delete_flight(airline_name: str, flight_num: str):
+async def delete_flight(airline: str, flight_num: str):
     
-    for airline in airline_list:
-        if airline.name == airline_name:
-            for i, flight in enumerate(airline.flights):
+    for airline_obj in airline_list:
+        if airline_obj.name == airline:
+            for i, flight in enumerate(airline_obj.flights):
                 if flight.flight_num == flight_num:
-                    deleted_flight = airline.flights.pop(i)
+                    deleted_flight = airline_obj.flights.pop(i)
                     return deleted_flight
     
     raise HTTPException(status_code=404, detail='Flight not found')
